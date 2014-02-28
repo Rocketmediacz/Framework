9e87c019df4c6018430a3d81caa9526b9a94336b
David Zadrazil <me@davidzadrazil.cz>
2014-02-28 16:47:41 +0100

    First ceremonial commit!

---
+++ Scribe/Application/UI/Control.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Copyright (c) 2014 David Zadražil (me@davidzadrazil.cz)
+ * 
+ * For the full copyright and license information, please view the file license.txt that was distributed with this source code.
+ */
+
+namespace Scribe\Application\UI;
+
+use Nette;
+
+/**
+ * Class Control
+ *
+ * @author David Zadražil <me@davidzadrazil.cz>
+ */
+abstract class Control extends Nette\Application\UI\Control
+{
+
+}
---
+++ Scribe/Application/UI/Presenter.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * Copyright (c) 2014 David Zadražil (me@davidzadrazil.cz)
+ *
+ * For the full copyright and license information, please view the file license.txt that was distributed with this source code.
+ */
+
+namespace Scribe\Application\UI;
+
+use Nette,
+	Nette\Application\ForbiddenRequestException;
+
+/**
+ * Class Presenter
+ *
+ * @author David Zadražil <me@davidzadrazil.cz>
+ */
+abstract class Presenter extends Nette\Application\UI\Presenter
+{
+
+	/** @persistent */
+	public $locale;
+
+	/** @var \Kdyby\Translation\Translator @inject */
+	public $translator;
+
+
+
+	/**
+	 * Check requirements
+	 *
+	 * @param $element
+	 */
+	public function checkRequirements($element)
+	{
+		if ($element->hasAnnotation('Secured')) {
+
+			// check user login
+			if (!$this->user->isLoggedIn()) {
+				$this->redirect("Uzivatel:prihlaseni", array("backlink" => $this->storeRequest()));
+			}
+
+			// check if user is in role
+			if ($element->hasAnnotation("Role")) {
+				if (!$user->isInRole($element->getAnnotation('Role'))) {
+					throw new Nette\Application\ForbiddenRequestException;
+				}
+			}
+
+			// check if user is allowed to do something...
+			if ($element->hasAnnotation("Resource")) {
+				if (!$user->isAllowed($element->getAnnotation("Resource"), $element->getAnnotation("Privilege"))) {
+					throw new Nette\Application\ForbiddenRequestException;
+				}
+			}
+
+		}
+	}
+
+}
