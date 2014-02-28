e51309ebab3e58fe9b740009d6d513f713f777cf
David Zadrazil <me@davidzadrazil.cz>
2014-02-28 16:51:23 +0100

    Removed dependency on Kdyby\Translator

--- Scribe/Application/UI/Presenter.php
+++ Scribe/Application/UI/Presenter.php
@@ -19,13 +19,6 @@ use Nette,
 abstract class Presenter extends Nette\Application\UI\Presenter
 {
 
-	/** @persistent */
-	public $locale;
-
-	/** @var \Kdyby\Translation\Translator @inject */
-	public $translator;
-
-
 
 	/**
 	 * Check requirements
